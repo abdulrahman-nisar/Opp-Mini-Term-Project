222d77e287918ed11b91097ff1e7fcf5546465ec
Danial-Zaheer <f233082@cfd.nu.edu.pk>
2024-04-20 15:37:02 +0500

    Just Copy_contact function is remaining

--- OutsideFunctionsImplementations.cpp
+++ OutsideFunctionsImplementations.cpp
@@ -3,25 +3,30 @@
 
 bool isStringOnlynumber(const string str)
 {
-	{
-		size_t length = str.length();
+	size_t length = str.length();			//Getting length of string
 
-		if (length < 11)
+	if (length < 11)			//Cheking if the string exceeds lenght of 11 digits beacuse we will be checkin only contact number 
+	{
+		return 1;
+	}
+	for (size_t i = 0; i < length; i++)
+	{
+		if (!(isdigit(str[i])))
 		{
 			return 1;
 		}
-		for (size_t i = 0; i < length; i++)
-		{
-			if (str[i] < 48 || str[i] > 57)
-			{
-				return 1;
-			}
-			return 0;
-		}
-
+		return 0;
 	}
 }
-bool isStringOnlyAlphbets(string& str)
+bool isStringOnlyAlphbets(const string str)
 {
-	return 1;
+	size_t length = str.length();				//Getting length of string
+	for (size_t i = 0; i < length; i++)			//Iterating through the string
+	{
+		if (!(isalpha(str[i])))				//Cheking if the character at every index is not an character
+		{
+			return 1;
+		}
+	}
+	return 0;
 }
